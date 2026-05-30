2c082b7d338d05d9aff57379f0890a7a75e3e0a8
jimmy-sketch <jimmy163mail@163.com>
2026-05-30 23:28:18 +0800

    feat: add WebSocket hello message validation

--- app/core/apis.py
+++ app/core/apis.py
@@ -1,8 +1,13 @@
+import json
+import platform
+
 from fastapi import APIRouter, WebSocket
 from loguru import logger
+from pydantic import ValidationError
 
 from app.db import SessionDep
 from app.models.adapters import Adapter
+from app.models.messages import Hello
 
 router = APIRouter(prefix="/adapter", tags=["adapter"])
 
@@ -10,4 +15,17 @@ router = APIRouter(prefix="/adapter", tags=["adapter"])
 @router.websocket("/ws")
 async def dispatch_messages(connection: WebSocket):
     await connection.accept()
-    platform
+    session = None
+    aid = None
+    try:
+        payload = await connection.receive_json()
+        message = Hello.model_validate(payload)
+        aid = message.aid
+        platform = message.platform
+
+    except ValidationError:
+        assert connection.client is not None  # 毋庸置疑！
+        logger.error(
+            f"Caught adapter from {connection.client.host}:{connection.client.port} does not send right hello message"
+        )
+        await connection.close(code=1008)
