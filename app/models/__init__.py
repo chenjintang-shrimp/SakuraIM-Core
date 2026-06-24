580cc54bf6488a2d82a495650ef4dbd963bdaf74
jimmy-sketch <jimmy163mail@163.com>
2026-06-24 17:01:11 +0800

    chore: remove dead code

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-import app.models.messages  # noqa: F401
-from app.models.adapters import Adapter
-from app.models.sessions import Session, SessionState
-from app.models.user import User
-
-__all__ = ["Session", "SessionState", "User", "Adapter", "messages"]
